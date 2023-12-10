1dbc9cb25a2038e72d0a2b03d2796feb35ace5ec
Tairraos <xiaole.tao@qq.com>
2023-12-10 16:52:11 +0800

    更新存档工具

--- api-archive.php
+++ api-archive.php
@@ -9,17 +9,6 @@ $lesson = $_REQUEST['lesson'];
 $theme = $_REQUEST['theme'];
 $stamp = date('Y-m-d H:i:s');
 
-// 把已经用过的目录和图片移动到archive存档
-rename('media/material', 'media/archive/' . $theme);
-rename('media/images/' . $theme . '.png', 'media/archive/' . $theme . '/' . $theme . '.png');
-
-// 创建新的空目录准备开始下一个工程
-mkdir('media/material');
-mkdir('media/material/audio');
-mkdir('media/material/video');
-mkdir('media/material/slide');
-
-
 $db = new PDO('sqlite:ttv-data.db');
 
 // 插入数据到archive表
@@ -33,6 +22,19 @@ $stmt->execute();
 
 // 删除material表中的数据
 $db->exec("DELETE FROM material;");
+
+// 把已经用过的目录移动到archive存档
+rename('media/material', 'media/archive/' . $theme);
+
+// 创建新的空目录准备开始下一个工程
+mkdir('media/material');
+mkdir('media/material/audio');
+mkdir('media/material/video');
+mkdir('media/material/slide');
+
+// 把已经用过的图片移动到archive存档
+rename('media/images/' . $theme . '.png', 'media/archive/' . $theme . '/' . $theme . '.png');
+
 
 header('Content-Type: application/json');
 echo json_encode(['result' => 'success'], JSON_UNESCAPED_UNICODE);
