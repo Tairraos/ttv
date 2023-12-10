7b9dafb48bc7988fe137cc9b33015057f9ddc81c
Tairraos <xiaole.tao@qq.com>
2023-12-10 16:39:54 +0800

    更新存档模块

--- api-archive.php
+++ api-archive.php
@@ -20,22 +20,22 @@ mkdir('media/material/video');
 mkdir('media/material/slide');
 
 
-// $db = new PDO('sqlite:ttv-data.db');
+$db = new PDO('sqlite:ttv-data.db');
 
-// // 插入数据到archive表
-// $stmt = $db->prepare("INSERT INTO archive (`id`, `lesson`, `type`, `group`, `chinese`, `english`, `phonetic`, `projectid`) SELECT `id`, `lesson`, `type`, `group`, `chinese`, `english`, `phonetic`, '$projectid' AS projectid FROM material");
-// $stmt->execute();
+// 插入数据到archive表
+$stmt = $db->prepare("INSERT INTO archive (`id`, `lesson`, `type`, `group`, `chinese`, `english`, `phonetic`, `projectid`) SELECT `id`, `lesson`, `type`, `group`, `chinese`, `english`, `phonetic`, '$projectid' AS projectid FROM material");
+$stmt->execute();
 
 
-// // 更新project表
-// $stmt = $db->prepare("UPDATE `project` SET `archived` = 'yes', `stamp` = '$stamp' WHERE projectid = $projectid AND lesson = '$lesson'");
-// $stmt->execute();
+// 更新project表
+$stmt = $db->prepare("UPDATE `project` SET `archived` = 'yes', `stamp` = '$stamp' WHERE projectid = $projectid AND lesson = '$lesson'");
+$stmt->execute();
 
-// // 删除material表中的数据
-// $db->exec("DELETE FROM material;");
+// 删除material表中的数据
+$db->exec("DELETE FROM material;");
 
-// header('Content-Type: application/json');
-// echo json_encode(['result' => 'success'], JSON_UNESCAPED_UNICODE);
+header('Content-Type: application/json');
+echo json_encode(['result' => 'success'], JSON_UNESCAPED_UNICODE);
 
-// $db = NULL;
+$db = NULL;
 ?>
