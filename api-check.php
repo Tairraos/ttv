dc6a67690ecbc4a0a045d7f135983da9b898d8be
Tairraos <xiaole.tao@qq.com>
2023-12-14 13:58:32 +0800

    修正bug，文件名空，检查应该返回no

--- api-check.php
+++ api-check.php
@@ -3,12 +3,17 @@
 // 检查文件是否存在
 // ************
 
-$filename = $_REQUEST['filename'] ?? "temp";
+$filename = $_REQUEST['filename'] ?? "";
 $ext = substr($filename, -4);
 $type = $ext == '.m4a' ? 'audio' : ($ext == '.mp4' ? 'video' : 'slide');
 $path = 'media/material/' . $type . '/' . $filename;
 
 header('Content-Type: application/json');
-echo json_encode(['result' => 'success', 'path' => $path, 'found' => file_exists($path) ? "yes" : "no"]);
+
+if ($filename == "") {
+    echo json_encode(['result' => 'success', 'path' => $path, 'found' => "no"]);
+} else {
+    echo json_encode(['result' => 'success', 'path' => $path, 'found' => file_exists($path) ? "yes" : "no"]);
+}
 
 ?>
