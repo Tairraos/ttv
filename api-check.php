36c38117a620e4a836f5d8fd86cdfaef309bf3aa
Tairraos <xiaole.tao@qq.com>
2023-12-17 00:31:14 +0800

    不指定文件名返回所有文件

--- api-check.php
+++ api-check.php
@@ -3,17 +3,23 @@
 // 检查文件是否存在
 // ************
 
-$filename = $_REQUEST['filename'] ?? "";
-$ext = substr($filename, -4);
-$type = $ext == '.m4a' ? 'audio' : ($ext == '.mp4' ? 'video' : 'slide');
-$path = 'media/material/' . $type . '/' . $filename;
+$filename = $_REQUEST['filename'] ?? '';
 
 header('Content-Type: application/json');
-
-if ($filename == "") {
-    echo json_encode(['result' => 'success', 'path' => $path, 'found' => "no"]);
+if ($filename != '') {
+    $ext = substr($filename, -4);
+    $type = $ext == '.m4a' ? 'audio' : ($ext == '.mp4' ? 'video' : 'slide');
+    $path = 'media/material/' . $type . '/' . $filename;
+    echo json_encode(['result' => 'success', 'path' => $path, 'found' => file_exists($path) ? 'yes' : 'no']);
 } else {
-    echo json_encode(['result' => 'success', 'path' => $path, 'found' => file_exists($path) ? "yes" : "no"]);
+    //返回 media/material/ 目录下的所有文件名列表
+    $audio = scandir('media/material/audio/');
+    $video = scandir('media/material/video/');
+    $slide = scandir('media/material/slide/');
+    $audio = array_values(array_filter($audio, fn($filename)=>preg_match('/^\d/', $filename)));
+    $video = array_values(array_filter($video, fn($filename)=>preg_match('/^\d/', $filename)));
+    $slide = array_values(array_filter($slide, fn($filename)=>preg_match('/^\d/', $filename)));
+    echo json_encode(['result' => 'success', 'files' => array_merge($audio,$video,$slide)]);
 }
 
 ?>
